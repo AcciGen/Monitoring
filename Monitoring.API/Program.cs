cbfae0822f5dbd6307a9fad5ee116526b87f6a88
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-07-27 17:32:45 +0500

    Program.cs moved to startup and layers configured

--- Monitoring.API/Program.cs
+++ Monitoring.API/Program.cs
@@ -7,30 +7,13 @@ namespace Monitoring.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            var startup = new Startup(builder.Configuration);
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            startup.ConfigureServices(builder.Services, builder.Logging);
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            startup.Configure(app, builder.Environment);
         }
     }
 }
---
+++ Monitoring.API/Startup.cs
@@ -0,0 +1,59 @@
+﻿namespace Monitoring.API
+{
+    public class Startup
+    {
+        public IConfiguration configRoot
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            configRoot = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services, ILoggingBuilder Logging)
+        {
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
+            //services.AddResumeAutoCheckkerBuissnessLogicDI(configRoot);
+            //services.AddResumeAutoCheckkerInfrastructureDI(configRoot);
+
+            services.AddMemoryCache();
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors();
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
